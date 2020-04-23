470ac7f9b5085017d4dd0118627b03f8d1ba69d6
sidsri1999 <61539851+sidsri1999@users.noreply.github.com>
2020-04-23 21:37:48 +0530

    Add files via upload
    Registration Form Application V1

---
+++ Registration Form v1/src/servlets/Noida.java
@@ -0,0 +1,51 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Noida extends HttpServlet {
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			response.setContentType("text/html");  
+		    PrintWriter out = response.getWriter();  
+		    String userName = request.getParameter("uname");
+			String password = request.getParameter("password");
+			String rPassword = request.getParameter("rPassword");
+			int age = Integer.parseInt(request.getParameter("age"));
+			String gender = request.getParameter("gender");
+			String city = request.getParameter("city");
+			String[] technologies = request.getParameterValues("technologies");
+			List<String> techno = Arrays.asList(technologies);
+			int wexp = Integer.parseInt(request.getParameter("wexp"));
+			
+			int techNo = technologies.length;
+			out.print("Name - "+userName+"<br>");
+			out.print("Age - "+age+"<br>");
+			out.print("Gender - "+gender+"<br>");
+			out.print("City - "+city+"<br>");
+			out.print("Technologies - ");
+			int i=0;
+			while(i!=techNo) {
+				out.print( technologies[i] + " ");
+				i++;
+			}
+			out.print("<br>");
+			out.print("Work Experience - "+wexp+" years<br>");
+			out.print("<br>");
+			out.print("<br>");
+			out.print("Jobs Availability : - ");
+			if(techno.contains("Python") && techno.contains("R")) {
+				out.print("AI Developer");
+			}else {
+				out.print("Sorry, No jobs available at this location");
+			}	
+	}
+
+}
---
+++ Registration Form v1/src/servlets/Validation.java
@@ -0,0 +1,78 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.regex.Pattern;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Validation extends HttpServlet {
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String userName = request.getParameter("uname");
+		String password = request.getParameter("password");
+		String rPassword = request.getParameter("rPassword");
+		int age=0;
+		try {
+			age = Integer.parseInt(request.getParameter("age"));
+		}catch(Exception e) {
+		}
+		String gender = request.getParameter("gender");
+		String city = request.getParameter("city");
+		String[] technologies = request.getParameterValues("technologies");
+		int wexp = Integer.parseInt(request.getParameter("wexp"));
+		User user = new User(userName,password,rPassword,age,gender,city,technologies,wexp);
+		String check = validation(user);
+		System.out.println("Check 1");
+		if(check.equals("fine")) {
+			System.out.println("Check 2");
+			if(city.equals("Greater Noida")) {
+				System.out.println("Check 21");
+				RequestDispatcher rd=request.getRequestDispatcher("GreaterNoida");  
+		        rd.forward(request, response);
+			}else if(city.equals("Noida")) {
+				RequestDispatcher rd=request.getRequestDispatcher("Noida");  
+		        rd.forward(request, response);
+			}else if(city.equals("Gurgaon")) {
+				RequestDispatcher rd=request.getRequestDispatcher("Gurgaon");  
+		        rd.forward(request, response);
+			}else if(city.equals("Delhi")) {
+				RequestDispatcher rd=request.getRequestDispatcher("Delhi");  
+		        rd.forward(request, response);
+			}
+		}else {
+			System.out.println("Check 3");
+			response.setContentType("text/html");
+			PrintWriter out = response.getWriter();
+			out.print("<h2>Invalid Credentials</h2>");
+			out.print("<h2>"+check+"</h2>");
+			out.print("<h2><a href='Registration.html\'>Re - Login</a></h2>");
+		}
+		
+	}
+	
+	public String validation(User user) {
+		String username = user.getUserName();
+		String password = user.getPassword();
+		String rPassword = user.getrPassword();
+		int age = user.getAge();
+		if(Pattern.matches("[a-zA-Z]{3,10}", username)) {
+			if(password.equals(rPassword)) {
+				if(age>=18 && age <=60) {
+					return "fine";
+				}else {
+					return "age should be between 18 to 60";
+				}
+			}else {
+				return "Password and Re-enter password not matched";
+			}
+		}else {
+			return "UserName should have atleast 3 characters and no special symbol";
+		}	
+	}
+
+}
